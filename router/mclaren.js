391ec8765260a56bd9cc5aedb2e3dac766fff5b4
pabloandrestorres1 <pablooat1@gmail.com>
2023-02-17 19:49:27 +0100

    js1

--- router/mclaren.js
+++ router/mclaren.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Piloto = require('../models/piloto');
+const Escuderia = require('../models/escuderia');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
@@ -8,9 +9,11 @@ router.get('/', async (req, res) => {
         //los datos que vienen de la base de datos
         //con respecto al arrayPokemon que tenemos EN LA VISTA
         const arrayPilotoDB = await Piloto.find({escuderia:'McLaren'});
+        const arrayEscuderiaDB = await Escuderia.find({nombre:'McLaren'});
         console.log(arrayPilotoDB);
         res.render("mclaren", { 
-            arrayPiloto: arrayPilotoDB
+            arrayPiloto: arrayPilotoDB,
+            arrayEscuderia: arrayEscuderiaDB
         })
     } catch (error) {
         console.error(error)
