e793bdec77d49c4059e731af276b19875245f571
pabloandrestorres1 <pablooat1@gmail.com>
2023-02-17 19:53:24 +0100

    js3

--- router/alpha.js
+++ router/alpha.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Piloto = require('../models/piloto');
+const Escuderia = require('../models/escuderia');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
@@ -8,9 +9,11 @@ router.get('/', async (req, res) => {
         //los datos que vienen de la base de datos
         //con respecto al arrayPokemon que tenemos EN LA VISTA
         const arrayPilotoDB = await Piloto.find({escuderia:'Alpha Tauri'});
+        const arrayEscuderiaDB = await Escuderia.find({nombre:'Alpha Tauri'});
         console.log(arrayPilotoDB);
         res.render("alpha", { 
-            arrayPiloto: arrayPilotoDB
+            arrayPiloto: arrayPilotoDB,
+            arrayEscuderia: arrayEscuderiaDB
         })
     } catch (error) {
         console.error(error)
