7d86994c0449bb66196faaf751d13a9c5549cc6f
pabloandrestorres1 <pablooat1@gmail.com>
2023-02-17 20:40:39 +0100

    fin

--- router/alfaromeo.js
+++ router/alfaromeo.js
@@ -5,9 +5,6 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        //Le pondremos arrayPokemonDB para diferenciar
-        //los datos que vienen de la base de datos
-        //con respecto al arrayPokemon que tenemos EN LA VISTA
         const arrayPilotoDB = await Piloto.find({escuderia:'Alfa Romeo'});
         const arrayEscuderiaDB = await Escuderia.find({nombre:'Alpha Tauri'});
         console.log(arrayPilotoDB);
