c341041a67a45ffbd1048909d44cf79fd836c8cb
Adrian Pascual <adrianpascual.20@campuscamara.es>
2023-02-19 15:05:41 +0100

    fin

--- router/alfaromeo.js
+++ router/alfaromeo.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     try {
         const arrayPilotoDB = await Piloto.find({escuderia:'Alfa Romeo'});
-        const arrayEscuderiaDB = await Escuderia.find({nombre:'Alpha Tauri'});
+        const arrayEscuderiaDB = await Escuderia.find({nombre:'Alfa Romeo'});
         res.render("alfaromeo", { 
             arrayPiloto: arrayPilotoDB,
             arrayEscuderia: arrayEscuderiaDB
